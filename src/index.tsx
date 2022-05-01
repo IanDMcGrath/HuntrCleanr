735bc1e7597a356a5f78a35fb4b769b3e7093b91
IanDMcGrath <ian92415@gmail.com>
2022-04-30 19:33:13 -0600

    working typescript react app no red squigglys

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
-import react from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app.tsx';
+import App from './app';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootDiv = document.getElementById('root');
 
   const root = ReactDOM.createRoot(rootDiv);
-  root.render(<App name="ian.tech" callback={() => console.log('app rendered');
+  root.render(<App/>);
 });
